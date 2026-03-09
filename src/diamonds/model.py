1f3aed39646c28b0d2266217e3c8385b74bfab3f
Aimen El Abidi <aelabidi@gmail.com>
2026-03-09 16:28:39 +0100

    Use of Predict function in Evaluate_Model

--- src/diamonds/model.py
+++ src/diamonds/model.py
@@ -2,6 +2,12 @@ import pandas as pd
 import time
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+    mean_absolute_percentage_error,
+)
 
 
 def create_model(model_name: str) -> BaseEstimator:
@@ -48,50 +54,6 @@ def train_model(model, X_train, y_train):
     print(f"Training time: {end_time - start_time} seconds")
 
 
-def evaluate_model(model, X_test, y_test) -> dict[str, float]:
-    """Evaluate the model on the test data.
-    Parameters
-    ----------
-    model : any
-        The trained model
-    X_test : pd.DataFrame
-        The test data
-    y_test : pd.Series
-        The true values
-
-    Returns
-    -------
-    dict[str, float]
-        The evaluation metrics
-    """
-    # NB : mae, mse, r2_score, mape
-    # Only print the metrics for now
-    # but return them as a dictionary for later use.
-    from sklearn.metrics import (
-        mean_absolute_error,
-        mean_squared_error,
-        r2_score,
-        mean_absolute_percentage_error,
-    )
-
-    y_pred = model.predict(X_test)  # Make predictions on the test data.
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    r2_score = r2_score(y_test, y_pred)
-    mape = mean_absolute_percentage_error(y_test, y_pred)
-    print("Metrics:")
-    print(f"MAE: {mae}")
-    print(f"MSE: {mse}")
-    print(f"R2 Score: {r2_score}")
-    print(f"MAPE: {mape}")
-    return {
-        "MAE": mae,
-        "MSE": mse,
-        "R2 Score": r2_score,
-        "MAPE": mape,
-    }  # Return the evaluation metrics as a dictionary.
-
-
 def predict(model, X):
     """
     Make predictions using the trained model.
@@ -113,3 +75,43 @@ def predict(model, X):
         y_pred, index=X.index
     )  # Convert to a pandas Series with the same index as the input data.
     return y_pred  # Return the predicted values as a pandas Series.
+
+
+def evaluate_model(model, X_test, y_test) -> dict[str, float]:
+    """Evaluate the model on the test data.
+    Parameters
+    ----------
+    model : any
+        The trained model
+    X_test : pd.DataFrame
+        The test data
+    y_test : pd.Series
+        The true values
+
+    Returns
+    -------
+    dict[str, float]
+        The evaluation metrics
+    """
+    # NB : mae, mse, r2_score, mape
+    # Only print the metrics for now
+    # but return them as a dictionary for later use.
+    # y_pred = model.predict(X_test)  # Make predictions on the test data
+    y_pred = predict(
+        model, X_test
+    )  # Use the predict function to make predictions on the test data.
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    r2score = r2_score(y_test, y_pred)
+    mape = mean_absolute_percentage_error(y_test, y_pred)
+    print("Metrics:")
+    print(f"MAE: {mae}")
+    print(f"MSE: {mse}")
+    print(f"R2 Score: {r2score}")
+    print(f"MAPE: {mape}")
+    return {
+        "MAE": mae,
+        "MSE": mse,
+        "R2 Score": r2score,
+        "MAPE": mape,
+    }  # Return the evaluation metrics as a dictionary.
