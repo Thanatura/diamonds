121e94790e07ddcf74c06c2948bf2a68a097478f
Nathan Casals <edouard.barbet@proton.me>
2026-03-09 16:13:40 +0000

    add train script (without persistence)

--- src/diamonds/train.py
+++ src/diamonds/train.py
@@ -1,3 +1,12 @@
+from diamonds.data import clean_data, create_X_y, load_data, preprocess_data, split_X_y
+from diamonds.model import (
+    create_model,
+    create_preproc,
+    evaluate_model,
+    train_model,
+)
+
+
 def train(
     model_name: str = "baseline",
     test_size: float = 0.2,
@@ -14,9 +23,24 @@ def train(
     """
     # 1) Data
 
+    df = load_data()
+    df_cleaned = clean_data(df)
+    df_preprocess = preprocess_data(df_cleaned)
+    X, y = create_X_y(df_preprocess)
+    X_train, X_test, y_train, y_test = split_X_y(X, y)
+
     # 2) Model + preprocessing
 
+    estimator = create_model("AdaBoostRegressor")
+    pre_processing = create_preproc()
+    pre_processing.fit(X_train)
+    X_train_scaled = pre_processing.transform(X_train)
+    X_test_scaled = pre_processing.transform(X_test)
+
     # 3) Evaluation
+
+    train_model(model=estimator, X_train=X_train_scaled, y_train=y_train)
+    evaluate_model(estimator, X_test_scaled, y_test)
 
     # 4) Persistence
 
